116109e098b95049d7be69085030c24f65bdb085
sacerdev <sacerdev@users.noreply.github.com>
2023-08-16 23:57:13 +0200

    more webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -5,37 +5,47 @@ const TerserWebpackPlugin = require('terser-webpack-plugin');
 const ZipWebpackPlugin = require('zip-webpack-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    index: './src/js/index.js',
-  },
-  output: {
-    filename: 'game.js',
-    path: path.resolve(__dirname, 'build'),
-  },
-  optimization: {
-    minimizer: [
-      new TerserWebpackPlugin(),
-      new CssMinimizerPlugin(),
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/html/index.html',
-      inject: 'body',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: './src/css', to: './' },
-        { from: './src/assets', to: './assets' },
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  return {
+    mode: options.mode,
+    entry: {
+      index: './src/js/index.js',
+    },
+    output: {
+      filename: 'game.js',
+      path: path.resolve(__dirname, 'build'),
+    },
+    optimization: {
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: isProduction,
+            },
+          },
+        }),
+        new CssMinimizerPlugin(),
       ],
-    }),
-    new ZipWebpackPlugin({
-      filename: 'entry.zip',
-      path: path.resolve(__dirname, 'dist'),
-      pathPrefix: 'build/',
-      threshold: 13000, // Set your threshold here
-    }),
-  ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/html/index.html',
+        inject: 'body',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './src/css', to: './' },
+          { from: './src/assets', to: './assets' },
+        ],
+      }),
+      new ZipWebpackPlugin({
+        filename: 'entry.zip',
+        path: path.resolve(__dirname, 'dist'),
+        pathPrefix: 'build/',
+        threshold: 13000, // Set your threshold here
+      }),
+    ],
+  };
 };
